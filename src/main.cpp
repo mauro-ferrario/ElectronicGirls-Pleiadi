fec8b98e817f9df6c52926963a3364e8d205e47e
mauro-ferrario <hello@mauroferrario.com>
2016-01-13 23:40:03 +0100

    Set not programmable renderer

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,14 @@
 #include "ofMain.h"
 #include "ofApp.h"
-
-#define USE_PROGRAMMABLE_RENDERER
-
-#ifdef USE_PROGRAMMABLE_RENDERER
-#include "ofGLProgrammableRenderer.h"
-#endif
+#include "ofAppGlutWindow.h"
 
 //========================================================================
 int main( ){
-  ofGLWindowSettings settings;
-  settings.setGLVersion(3,2);
-  ofCreateWindow(settings);     // this kicks off the running of my app
-  ofRunApp(new ofApp());
+  ofAppGlutWindow window;
+  ofSetupOpenGL(&window, 800,600, OF_WINDOW);			// <-------- setup the GL context
+  
+  // this kicks off the running of my app
+  // can be OF_WINDOW or OF_FULLSCREEN
+  // pass in width and height too:
+  ofRunApp( new ofApp());
 }
